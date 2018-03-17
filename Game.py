c7f0ab81cecfe07e9564e28649648cbcf8f65a97
coon126 <32343723+coon126@users.noreply.github.com>
2018-03-17 03:25:51 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -2,21 +2,114 @@ import random
 
 score = 0
 
-def main():
+def plus():
     global score
     min = 0
     max = 100
     a = random.randint(min,max)
     b = random.randint(min,max)
-    print a ,'+', b ,'= ?'
-    sum = input('Plz enter you answer: ')
 
-    if a + b == sum:
-        print 'Correct'
+    print a ,'+', b,' = ?'
+    c = input('Plz Enter Your Answer: ')
+
+    if a + b == c:
+        print 'Correct Add Point'
         score = score + 1
-        print 'Your score is: ',score
+        print 'Your New Score Is: ',score
     else:
-        print 'Plz try agin'
+        print 'Incorrect Remove point'
+        score = score - 1
+        print 'Your New Score Is: ',score
+
+def subtraction():
+    global score
+    min = 0
+    max = 100
+    a = random.randint(min, max)
+    b = random.randint(min, max)
+    print a, '-', b, ' = ?'
+    c = input('Plz Enter Your Answer: ')
+
+    if a - b == c:
+        print 'Correct Add Point'
+        score = score + 1
+        print 'Your New Score Is: ', score
+    else:
+        print 'Incorrect Remove point'
+        score = score - 1
+        print 'Your New Score Is: ', score
+
+def multiplication():
+    global score
+    min = 0
+    max = 100
+    a = random.randint(min, max)
+    b = random.randint(min, max)
+    print a, '*', b, ' = ?'
+    c = input('Plz Enter Your Answer: ')
+
+    if a * b == c:
+        print 'Correct Add Point'
+        score = score + 1
+        print 'Your New Score Is: ', score
+    else:
+        print 'Incorrect Remove point'
+        score = score - 1
+        print 'Your New Score Is: ', score
+
+def division():
+    global score
+    min = 0
+    max = 100
+    a = float(random.randint(min, max))
+    b = float(random.randint(min, max))
+    print a, '/', b, ' = ?'
+    c = float(input('Plz Enter Your Answer: '))
+
+    if a / b == c:
+        print 'Correct Add Point'
+        score = score + 1
+        print 'Your New Score Is: ', score
+    else:
+        print 'Incorrect Remove point'
+        score = score - 1
+        print 'Your New Score Is: ', score
+        print a/b
+
+def menu():
+    print '''
+    +------|Practice Calculations|------+
+    |                                    |
+    |              Walcom!               |
+    |   Today we Practice calculations   |
+    |   Please select what are you want  |
+    |   To Practice:                     |
+    |                                    |
+    |          1. plus                   |
+    |          2. subtraction            |
+    |          3. multiplication         |
+    |          4. division               |
+    |          5. exit
+    |                                    |
+    +------------------------------------+
+    '''
+    select = raw_input("please select number:")
+    print select
+    if select == '1' or select == 'plus' or select == 'p':
+        while(True):
+            plus()
+    elif select == '2' or select == 'multiplication'or select == 'm':
+        while (True):
+            subtraction()
+    elif select == '3' or select == 'division' or select == 'd':
+        while(True):
+            multiplication()
+    elif select == '4' or select == 'subtraction' or select == 's':
+        while(True):
+            division()
+    elif select == '5'\
+            or select == 'exit' or select == 'e':
+        exit()
 
 while(True):
-    main()
+    menu()
